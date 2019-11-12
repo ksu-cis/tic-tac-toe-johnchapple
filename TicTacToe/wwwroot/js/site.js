b0c76b1bd2bc45336514912a74d5aadb5f4fdbcf
John Chapple <johnchapple@win.cis.ksu.edu>
2019-11-12 15:29:53 -0600

    3:29 PM

--- TicTacToe/wwwroot/js/site.js
+++ TicTacToe/wwwroot/js/site.js
@@ -2,3 +2,22 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your Javascript code.
+var currentTurn = "X";
+
+var turnElement = document.getElementById("turn");
+turnElement.innerText = `Player ${currentTurn}'s turn`;
+
+var squares = document.getElementsByClassName("square");
+for (var i = 0; i < squares.length; i++) {
+    squares[i].addEventListener('click', onClick);
+}
+
+function onClick(event) {
+    event.preventDefault();
+    event.target.innerText = currentTurn
+    nextTurn()
+}
+
+function nextTurn() {
+    currentTurn = (currentTurn === "X") ? "O" : "X";
+}
